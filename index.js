1d8c6e2577f7f9f1e4a7d38c57dd91072d6b2523
Zhu Shaoying <shaoying.zhu@shopee.com>
2023-02-15 16:16:43 +0800

    control another tab

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const Koa = require('koa');
+const Koa = require('koa')
 const KoaStatic = require('koa-static')
-const app = new Koa();
-const path = require("path")
+const app = new Koa()
+const path = require('path')
 
 app.use(KoaStatic(path.posix.join(__dirname, 'static')))
 
+console.log('open http://localhost:9090')
 app.listen(9090)
